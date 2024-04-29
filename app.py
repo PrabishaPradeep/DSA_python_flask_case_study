7c1ad17363b1c9a6631b8b353b3c62e759b6d730
PrabishaPradeep <156564845+PrabishaPradeep@users.noreply.github.com>
2024-04-29 21:51:00 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask,request,render_template
+import pickle
+import numpy as np
+
+with open('model.pkl','rb') as model_file:
+    model=pickle.load(model_file)  
+
+app=Flask(__name__)
+                      
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+@app.route('/prediction',methods=['POST'])
+def predict():
+   Gend=request.form['Gender']
+   if Gend=="Male":
+     Gender=1
+   elif Gend=='Female':
+     Gender=0
+   EstimatedSalary=int(request.form['EstimatedSalary'])
+   Age=int(request.form['Age'])
+   feature=np.array([[Gender,Age,EstimatedSalary]])
+   prediction=model.predict(feature)
+   return render_template('index.html',pred_res=prediction[0])
+
+   
+if __name__=='__main__':
+  app.run(debug=True)
